560c3afb1fb4b8f70feb2fcf9215206e685d4597
onur94 <onuur94@hotmail.com>
2021-02-04 22:48:56 +0300

    Add files via upload

--- Full_Adder_2.vhd
+++ Full_Adder_2.vhd
@@ -4,8 +4,8 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity Full_Adder_2 is
 	port 
 	(
-		a 			: in std_logic;
-		b 			: in std_logic;
+		a 		: in std_logic;
+		b 		: in std_logic;
 		carry_in 	: in std_logic;
 		carry_out 	: out std_logic;
 		sum 		: out std_logic
