697c449d8f9b40dd2700faa478effb28c2d01ce6
onur94 <onuur94@hotmail.com>
2021-02-07 00:12:28 +0300

    Update test.vhd

--- test.vhd
+++ test.vhd
@@ -1,38 +1,39 @@
-Library IEEE;
-Use IEEE.STD_LOGIC_1164.All;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
 
-Entity Button_debouncer Is
-    Port (
-        input  : In     STD_LOGIC;
-        clk    : In     STD_LOGIC;
-        output : Buffer STD_LOGIC := '0'
+ENTITY Button_debouncer IS
+    PORT
+    (
+        input  : IN     STD_LOGIC;
+        clk    : IN     STD_LOGIC;
+        output : BUFFER STD_LOGIC := '0'
     );
-End Button_debouncer;
+END Button_debouncer;
 
-Architecture Behavioral Of Button_debouncer Is
-Begin
-    Process (input, clk)
-        Variable counter : INTEGER Range 0 To 50000000 := 0;
-        Variable pressed : BOOLEAN;
-    Begin
-        If rising_edge(clk) Then
-            If input = '1' Then
+ARCHITECTURE Behavioral OF Button_debouncer IS
+BEGIN
+    PROCESS (input, clk)
+        VARIABLE counter : INTEGER RANGE 0 TO 50000000 := 0;
+        VARIABLE pressed : BOOLEAN;
+    BEGIN
+        IF rising_edge(clk) THEN
+            IF input = '1' THEN
                 pressed := true;
-            Else
+            ELSE
                 output <= '0';
-            End If;
-            If pressed = true Then
+            END IF;
+            IF pressed = true THEN
                 counter := counter + 1;
-                If counter = 2500000 Then
+                IF counter = 2500000 THEN
                     counter := 0;
                     pressed := false;
-                    If input = '1' Then
+                    IF input = '1' THEN
                         output <= '1';
-                    Else
+                    ELSE
                         output <= '0';
-                    End If;
-                End If;
-            End If;
-        End If;
-    End Process;
-End Behavioral;
+                    END IF;
+                END IF;
+            END IF;
+        END IF;
+    END PROCESS;
+END Behavioral;
