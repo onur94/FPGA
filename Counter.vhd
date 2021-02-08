44ff33e67ea15276257ed08aa81183f35f250755
onur94 <onuur94@hotmail.com>
2021-02-08 10:11:49 +0300

    Add files via upload

--- Counter.vhd
+++ Counter.vhd
@@ -14,15 +14,15 @@ end Counter;
 architecture Behavioral of Counter is
     signal counter      : unsigned(3 downto 0) := (others => '0');
     signal counter_2    : unsigned(3 downto 0) := (others => '0');
-    constant maxcount   : integer              := 50000000;
-    constant maxcount_2 : integer              := 15;
+    constant maxcount   : integer              := 4;
+    constant maxcount_2 : integer              := 5;
 begin
     count <= std_logic_vector(counter_2);
 
     counter_proc : process (clk)
     begin
-        if (rising_edge(clk)) then
-            if (reset = '1' or counter = maxcount) then
+        if rising_edge(clk) then
+            if reset = '1' or counter = maxcount then
                 counter <= (others => '0');
             else
                 counter <= counter + 1;
@@ -32,10 +32,10 @@ begin
 
     counter_proc_2 : process (clk)
     begin
-        if (rising_edge(clk)) then
-            if (reset = '1' or counter_2 = maxcount_2) then
+        if rising_edge(clk) then
+            if reset = '1' or counter_2 = maxcount_2 then
                 counter_2 <= (others => '0');
-            elsif (counter = maxcount) then
+            elsif counter = maxcount then
                 counter_2 <= counter_2 + 1;
             end if;
         end if;
