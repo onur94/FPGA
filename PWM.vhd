3d1fa5f1243335886b9df8473c3da297f3c89da6
onur94 <onuur94@hotmail.com>
2021-02-12 20:28:25 +0300

    Add files via upload

---
+++ PWM.vhd
@@ -0,0 +1,52 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use ieee.std_logic_unsigned.all;
+
+entity PWM is
+    generic (
+        clk_freq       : integer := 50000000;
+        pwm_freq       : integer := 1000;
+        pwm_resolution : integer := 16
+    );
+    port
+    (
+        clk     : in  std_logic;
+        rst     : in  std_logic;
+        enable  : in  std_logic;
+        duty    : in  std_logic_vector(pwm_resolution - 1 downto 0); -- 65535
+        pwm_out : out std_logic
+    );
+end PWM;
+
+architecture Behavioral of PWM is
+    constant period 		: integer := clk_freq/pwm_freq;
+    signal count 			: integer range 0 to period + 1 := 0;
+    signal pwm_out_reg	: std_logic := '0';
+
+begin
+    pwm_out <= pwm_out_reg;
+    process (clk)
+    begin
+        if rst = '1' then
+            count <= 0;
+            pwm_out_reg <= '0';
+        elsif rising_edge(clk) then
+            if enable = '1' then
+                count <= count + 1;
+                if count <= period - 1 then
+                    if count <= duty - 1 then
+                        pwm_out_reg <= '1';
+                    elsif count >= duty - 1 then
+                        pwm_out_reg <= '0';
+                    end if;
+                else
+                    count <= 1;
+                    pwm_out_reg <= '1';
+                end if;
+            else
+                count <= 0;
+                pwm_out_reg <= '0';
+            end if;
+        end if;
+    end process;
+end Behavioral;
---
+++ tb_PWM.vhd
@@ -0,0 +1,47 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use ieee.numeric_std.all;
+
+entity tb_PWM is
+end tb_PWM;
+
+architecture tb of tb_PWM is
+    component PWM is
+        generic (
+            clk_freq       : integer := 50000000;
+            pwm_freq       : integer := 1000;
+            pwm_resolution : integer := 16
+        );
+        port
+        (
+            clk     : in  std_logic;
+            rst     : in  std_logic;
+            enable  : in  std_logic;
+            duty    : in  std_logic_vector(pwm_resolution - 1 downto 0);
+            pwm_out : out std_logic
+        );
+    end component;
+    signal clk		 : std_logic := '0';
+    signal rst		 : std_logic := '0';
+    signal enable	 : std_logic := '1';
+	 signal duty	 : std_logic_vector(16 - 1 downto 0) := std_logic_vector(to_unsigned(12500, 16));
+    signal pwm_out : std_logic;
+
+begin
+    clk_stimulus : process
+    begin
+        clk <= clk xor '1';
+        wait for 1 ps;
+    end process;
+
+    uut : PWM port map
+        (clk, rst, enable, duty, pwm_out);
+
+    data_stimulus : process
+    begin
+        rst <= '0';
+        wait for 400003 ps;
+
+        assert false report "Test: OK" severity failure;
+    end process;
+end tb;
