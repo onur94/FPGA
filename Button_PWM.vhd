48037150af2501231dad0c5f78f8af6778c207c1
onur94 <onuur94@hotmail.com>
2021-02-18 20:43:54 +0300

    Add files via upload

--- Button_PWM.vhd
+++ Button_PWM.vhd
@@ -1,13 +1,13 @@
 library IEEE;
-use IEEE.STD_LOGIC_1164.all;
-use IEEE.NUMERIC_STD.all;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 entity Button_PWM is
     port
     (
         button : in  std_logic;
         rst    : in  std_logic;
-        duty   : out std_logic_vector (15 downto 0) -- 50000
+        duty   : out std_logic_vector (7 downto 0) -- 50000
     );
 end Button_PWM;
 
@@ -15,7 +15,7 @@ architecture Behavioral of Button_PWM is
     type my_states is (Level_1, Level_2, Level_3, Level_4, Level_5, Level_6);
     signal state : my_states;
 
-    signal output : std_logic_vector (15 downto 0) := (others => '0');
+    signal output : std_logic_vector (7 downto 0) := (others => '0');
 begin
     duty <= output;
 
@@ -27,27 +27,27 @@ begin
         elsif rising_edge(button) then
             case state is
                 when Level_1 =>
-                    output <= std_logic_vector(to_unsigned(10000, 16));
+                    output <= std_logic_vector(to_unsigned(51, 8));
                     state <= Level_2;
 
                 when Level_2 =>
-                    output <= std_logic_vector(to_unsigned(20000, 16));
+                    output <= std_logic_vector(to_unsigned(102, 8));
                     state <= Level_3;
 
                 when Level_3 =>
-                    output <= std_logic_vector(to_unsigned(30000, 16));
+                    output <= std_logic_vector(to_unsigned(153, 8));
                     state <= Level_4;
 
                 when Level_4 =>
-                    output <= std_logic_vector(to_unsigned(40000, 16));
+                    output <= std_logic_vector(to_unsigned(204, 8));
                     state <= Level_5;
 
                 when Level_5 =>
-                    output <= std_logic_vector(to_unsigned(50000, 16));
+                    output <= std_logic_vector(to_unsigned(255, 8));
                     state <= Level_6;
 
                 when Level_6 =>
-                    output <= std_logic_vector(to_unsigned(0, 16));
+                    output <= std_logic_vector(to_unsigned(0, 8));
                     state <= Level_1;
 
                 when others =>
--- PWM.vhd
+++ PWM.vhd
@@ -1,13 +1,13 @@
 library IEEE;
-use IEEE.STD_LOGIC_1164.all;
-use IEEE.STD_LOGIC_UNSIGNED.all;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity PWM is
     generic 
-    (
-        clk_freq       : integer := 50000000;
+	(
+        clk_freq       : integer := 50_000_000;
         pwm_freq       : integer := 1000;
-        pwm_resolution : integer := 16
+        pwm_resolution : integer := 8
     );
     port
     (
@@ -20,30 +20,51 @@ entity PWM is
 end PWM;
 
 architecture Behavioral of PWM is
-    constant period : integer := clk_freq/pwm_freq;
+    constant period : integer := 2**pwm_resolution - 1;
+	constant clk_divider : integer := clk_freq/pwm_freq/period/2;
+	
     signal count : integer range 0 to period + 1 := 0;
     signal pwm_out_reg : std_logic := '0';
+	
+	signal divided_count : integer range 0 to clk_divider := 0;
+	signal pwm_clock : std_logic := '0';
 
 begin
+	process (clk)
+	begin
+		if rising_edge(clk) then
+			divided_count <= divided_count + 1;
+			if divided_count = clk_divider then
+				pwm_clock <= pwm_clock XOR '1';
+				divided_count <= 0;
+			end if;
+		end if;
+	end process;
+	
     pwm_out <= pwm_out_reg;
-    process (clk)
+	
+	process (pwm_clock)
     begin
         if rst = '1' then
             count <= 0;
             pwm_out_reg <= '0';
-        elsif rising_edge(clk) then
+        elsif rising_edge(pwm_clock) then
             if enable = '1' then
-                count <= count + 1;
-                if count < period then
-                    if count < duty then
-                        pwm_out_reg <= '1';
-                    elsif count >= duty then
-                        pwm_out_reg <= '0';
-                    end if;
-                else
-                    count <= 1;
-                    pwm_out_reg <= '1';
-                end if;
+				if duty /= x"0" then
+					count <= count + 1;
+					if count < period then
+						if count < duty then
+							pwm_out_reg <= '1';
+						elsif count >= duty then
+							pwm_out_reg <= '0';
+						end if;
+					else
+						count <= 1;
+						pwm_out_reg <= '1';
+					end if;
+				else
+					pwm_out_reg <= '0';
+				end if;
             else
                 count <= 0;
                 pwm_out_reg <= '0';
