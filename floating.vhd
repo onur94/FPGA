517fc37e992dd710790b417695c0d5d620738ce9
onur94 <onuur94@hotmail.com>
2022-02-26 18:28:13 +0300

    Add files via upload

--- floating.vhd
+++ floating.vhd
@@ -1,29 +1,29 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use work.float_pkg.all;
-use ieee.std_logic_arith.all;
 use IEEE.numeric_std.all;
 
-entity floating is
+entity Floating is
   Generic (
-    constant adc_resolution : real := 0.000000298023223876953125
+    constant pi : real := 3.14
   );
   Port (
     clk : in std_logic;
-    input_x : in integer;
-    data_out : out STD_LOGIC_VECTOR ( 31 downto 0 )
+    input_x : in integer
   );
-end floating;
+end Floating;
 
-architecture Behavioral of floating is
-    signal float_slv : STD_LOGIC_VECTOR ( 31 downto 0 );
+architecture Behavioral of Floating is
+    signal a : STD_LOGIC_VECTOR ( 31 downto 0 );
+    signal b : STD_LOGIC_VECTOR ( 31 downto 0 );
+    signal c : STD_LOGIC_VECTOR ( 31 downto 0 );
     --constant float_real : real := 1.2; -- Synthesizable
     --signal float_real : real;          -- Non-Synthesizable
 begin
     process (clk)
     begin
-        --x <= x * 2.0;
-        float_slv <= to_slv(to_float(input_x)*adc_resolution);
-        --float_real <= to_real(to_float(input_x))*adc_res;
+        a <= to_slv(to_float(input_x)*pi);
+        b <= to_slv(to_float(a)*2);
+        c <= to_slv(to_float(b)+5.5);
     end process;
 end Behavioral;
